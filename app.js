979d28a466c0cb9a5dd095c86705203c1d790e03
skan7575 <Ubiquity LOCOM5>
2022-10-03 12:50:22 +0300

    Сделана валидация

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { celebrate, Joi } = require('celebrate');
 const routerUsers = require('./routes/users');
 const routerCards = require('./routes/cards');
 const { NOT_FOUND, BAD_REQUEST } = require('./errors/error_codes');
@@ -27,11 +28,25 @@ const errorNotFound = (req, res) => {
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb');
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
 
 app.use('/cards', auth, routerCards);
 app.use('/users', auth, routerUsers);
-app.post('/signin', login);
-app.post('/signup', createUser);
 
 app.use(errorNotFound);
 app.use(handleErrors);
