7091cfcd72daf37036ef10008498a7d824d689d0
skan7575 <Ubiquity LOCOM5>
2022-10-03 13:11:06 +0300

    Проверка далее

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,34 +1,39 @@
 const { BAD_REQUEST, SIGN_UP_ERROR } = require('../errors/error_codes');
 
-module.exports.handleErrors = (err, req, res, next) => {
+module.exports.handleErrors = (err, res, next) => {
   const { statusCode = 500, message } = err;
-
-  if (err.name === 'CastError') {
-    res
-      .status(BAD_REQUEST)
-      .send({ message: 'Невалидный id' });
-  } else {
-    res
-      .status(statusCode)
-      .send({ message: statusCode === 500 ? 'Ошибка работы сервера' : message });
-  }
+  res.status(statusCode).send({ message: statusCode === 500 ? 'Ошибка работы сервера' : message });
   next();
-  if (err.name === 'ValidationError' || err.name === 'CastError') {
-    res
-      .status(BAD_REQUEST)
-      .send({
-        message: 'Некорректные данные',
-      });
+}
 
-    next();
-  }
-  if (err.code === 11000) {
-    res
-      .status(SIGN_UP_ERROR)
-      .send({
-        message: 'Пользователь уже зарегистрирован',
-      });
+// module.exports = handleErrors;
 
-    next();
-  }
-};
+// module.exports.handleErrors = (err, req, res) => {
+//   const { statusCode = 500, message } = err;
+//
+//   if (err.name === 'CastError') {
+//     res
+//       .status(BAD_REQUEST)
+//       .send({ message: 'Невалидный id' });
+//   } else {
+//     return res
+//       .status(statusCode)
+//       .send({ message: statusCode === 500 ? 'Ошибка работы сервера' : message });
+//   }
+//   if (err.name === 'ValidationError' || err.name === 'CastError') {
+//     res
+//       .status(BAD_REQUEST)
+//       .send({
+//         message: 'Некорректные данные',
+//       });
+//
+//   }
+//   if (err.code === 11000) {
+//     res
+//       .status(SIGN_UP_ERROR)
+//       .send({
+//         message: 'Пользователь уже зарегистрирован',
+//       });
+//
+//   }
+// };
