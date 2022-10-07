d35fd7429629e4c984994ff5761e28e52debc233
skan7575 <Ubiquity LOCOM5>
2022-10-07 12:43:44 +0300

    добавил cors разрешил всем и сложные запросы

--- app.js
+++ app.js
@@ -28,7 +28,16 @@ app.use(function(req, res, next) {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', '*');
   }
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
+// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+
+// Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    return res.end();
+  }
   next();
 });
 app.use(bodyParser.json());
