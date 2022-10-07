2b7a0bb56af86f94169037b34aae3e94e2cf7e53
skan7575 <Ubiquity LOCOM5>
2022-10-07 16:35:46 +0300

    1234

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(function(req, res, next) {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', origin);
   }
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
