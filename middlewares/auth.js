e96af8e38ce3a613605b24dc573a1f9d33dec341
skan7575 <Ubiquity LOCOM5>
2022-10-11 15:01:39 +0300

    ENV борьба

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,3 @@
-const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const { UnauthorizedError } = require('../errors/UnauthorizedError');
 
@@ -14,7 +13,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret-key');
+    payload = jwt.verify(token, 'PrivateKey');
   } catch (err) {
     next(new UnauthorizedError('Необходима авторизация'));
     return;
