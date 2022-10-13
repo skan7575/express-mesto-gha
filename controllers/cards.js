c8e5da2ef4fb23aa086429e531405b086f1fbfaa
skan7575 <Ubiquity LOCOM5>
2022-10-13 13:01:36 +0300

    Сделал правильный ответ owner

--- controllers/cards.js
+++ controllers/cards.js
@@ -9,7 +9,7 @@ const createCard = (req, res, next) => {
   const owner = req.user._id;
 
   Card.create({ name, link, owner })
-    .then((card) => res.status(201).send({card}))
+    .then((card) => res.status(201).send(card.populate('owner')))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new ValidationError('Введены не некорректные данные'));
